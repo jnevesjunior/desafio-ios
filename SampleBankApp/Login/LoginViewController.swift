17786c3e06e9379e70e15b1629104a2a9752b4af
Jose Neves <contato@joseneves.com.br>
2021-03-08 19:36:44 -0300

    Code smells

--- SampleBankApp/Login/LoginViewController.swift
+++ SampleBankApp/Login/LoginViewController.swift
@@ -10,12 +10,28 @@ import MaterialComponents.MaterialTextControls_OutlinedTextFields
 import MaterialComponents.MaterialTextControls_OutlinedTextFieldsTheming
 
 class LoginViewController: UIViewController {
+    
+    var loginTextField: MDCOutlinedTextField?
+    var passTextField: MDCOutlinedTextField?
 
     override func loadView() {
         super.loadView()
 
         self.view.backgroundColor = UIColor(named: "Primary")
 
+        
+        self.addLogoImg()
+
+        self.addLoginTextField()
+        self.addPassTextField()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    private func addLogoImg() {
         let logoImageView = UIImageView(image: UIImage(named: "Logo"))
         self.view.addSubview(logoImageView)
 
@@ -34,34 +50,35 @@ class LoginViewController: UIViewController {
                            attribute: .topMargin,
                            multiplier: 1,
                            constant: 50).isActive = true
-
-
+    }
+    
+    private func addLoginTextField() {
         let estimatedFrame = CGRect(x: 0, y: 0, width: 100, height: 55)
-        let phoneTextField = MDCOutlinedTextField(frame: estimatedFrame)
-        phoneTextField.label.text = "Nome ou email"
-        phoneTextField.sizeToFit()
-        phoneTextField.applySBATheme()
+        let textField = MDCOutlinedTextField(frame: estimatedFrame)
+        textField.label.text = "Nome ou email"
+        textField.sizeToFit()
+        textField.applySBATheme()
         
-        phoneTextField.setLeadingIcon(UIImage(named: "Person")!)
+        textField.setLeadingIcon(UIImage(named: "Person")!)
         
-        self.view.addSubview(phoneTextField)
+        self.view.addSubview(textField)
 
-        phoneTextField.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint(item: phoneTextField,
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: textField,
                            attribute: .leading,
                            relatedBy: .equal,
                            toItem: view,
                            attribute: .leading,
                            multiplier: 1,
                            constant: 20).isActive = true
-        NSLayoutConstraint(item: phoneTextField,
+        NSLayoutConstraint(item: textField,
                            attribute: .trailing,
                            relatedBy: .equal,
                            toItem: view,
                            attribute: .trailing,
                            multiplier: 1,
                            constant: -20).isActive = true
-        NSLayoutConstraint(item: phoneTextField,
+        NSLayoutConstraint(item: textField,
                            attribute: .top,
                            relatedBy: .equal,
                            toItem: view,
@@ -69,48 +86,46 @@ class LoginViewController: UIViewController {
                            multiplier: 1,
                            constant: 200).isActive = true
         
-        let passTextField = MDCOutlinedTextField(frame: estimatedFrame)
-        passTextField.label.text = "Senha"
-        passTextField.isSecureTextEntry = true
-        passTextField.sizeToFit()
-        passTextField.applySBATheme()
+        loginTextField = textField
+    }
+    
+    private func addPassTextField() {
+        let estimatedFrame = CGRect(x: 0, y: 0, width: 100, height: 55)
+        let textField = MDCOutlinedTextField(frame: estimatedFrame)
+        textField.label.text = "Senha"
+        textField.isSecureTextEntry = true
+        textField.sizeToFit()
+        textField.applySBATheme()
         
-        passTextField.setLeadingIcon(UIImage(named: "Padlock")!)
+        textField.setLeadingIcon(UIImage(named: "Padlock")!)
         
-        self.view.addSubview(passTextField)
+        self.view.addSubview(textField)
 
-        passTextField.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint(item: passTextField,
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: textField,
                            attribute: .leading,
                            relatedBy: .equal,
                            toItem: view,
                            attribute: .leading,
                            multiplier: 1,
                            constant: 20).isActive = true
-        NSLayoutConstraint(item: passTextField,
+        NSLayoutConstraint(item: textField,
                            attribute: .trailing,
                            relatedBy: .equal,
                            toItem: view,
                            attribute: .trailing,
                            multiplier: 1,
                            constant: -20).isActive = true
-        NSLayoutConstraint(item: passTextField,
+        NSLayoutConstraint(item: textField,
                            attribute: .top,
                            relatedBy: .equal,
-                           toItem: phoneTextField,
+                           toItem: self.loginTextField!,
                            attribute: .bottomMargin,
                            multiplier: 1,
                            constant: 30).isActive = true
-
-
+        
+        passTextField = textField
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-
-    }
-
 
 }
 
